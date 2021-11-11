c20179f5a417257614cfc13370b4367971070265
alllef <alekz29@gmail.com>
2021-11-11 15:19:34 +0200

    Added some simple methods to client

--- src/main/java/com/github/alllef/brokerfirmservice/service/ClientService.java
+++ src/main/java/com/github/alllef/brokerfirmservice/service/ClientService.java
@@ -2,14 +2,23 @@ package com.github.alllef.brokerfirmservice.service;
 
 import com.github.alllef.brokerfirmservice.entity.Flat;
 import com.github.alllef.brokerfirmservice.entity.person.Client;
+import com.github.alllef.brokerfirmservice.repository.ClientRepo;
+import com.github.alllef.brokerfirmservice.repository.FlatRepo;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class ClientService {
+    private final ClientRepo clientRepo;
+    private final FlatRepo flatRepo;
 
-    public void createClient(Client client){}
-    public void createFlat(Flat flat){
+    public void saveClient(Client client) {
+        clientRepo.save(client);
+    }
 
+    public void createFlat(Flat flat) {
+        flatRepo.save(flat);
     }
 
 }
