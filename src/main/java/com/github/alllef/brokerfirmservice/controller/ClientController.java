aa47ab1832072ff4b1fb2443d4bebae4cc694873
alllef <alekz29@gmail.com>
2021-11-11 17:07:16 +0200

    Added CRUD rest operations to ClientController

--- src/main/java/com/github/alllef/brokerfirmservice/controller/ClientController.java
+++ src/main/java/com/github/alllef/brokerfirmservice/controller/ClientController.java
@@ -3,9 +3,7 @@ package com.github.alllef.brokerfirmservice.controller;
 import com.github.alllef.brokerfirmservice.entity.person.Client;
 import com.github.alllef.brokerfirmservice.repository.ClientRepo;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +16,20 @@ public class ClientController {
     @GetMapping
     List<Client> getAll() {
         return clientRepo.findAll();
+    }
+
+    @PostMapping
+    void createClient(@RequestBody Client client){
+        clientRepo.save(client);
+    }
+
+    @PutMapping("/{id}")
+    void updateClient(@PathVariable Long id,@RequestBody Client client){
+        clientRepo.save(client);
+    }
+
+    @DeleteMapping("/{id}")
+    void deleteClient(@PathVariable Long id) {
+        clientRepo.deleteById(id);
     }
 }
